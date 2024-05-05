fd9d8a92984152656be3ac6c74c936ebff6014ca
MateusLeonardo <mateus_leonardo1997@hotmail.com>
2024-05-04 21:37:14 -0300

    feat: add logo

---
+++ src/components/logo.tsx
@@ -0,0 +1,8 @@
+export const Logo = () => {
+  return (
+    <div className="text-xl flex items-center gap-1">
+      <span className="block">Loja</span>
+      <span className="font-bold block">Zap</span>
+    </div>
+  );
+};
