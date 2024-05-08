15f5d1a892fc2806a650d468297f88f8380556fb
MateusLeonardo <mateus_leonardo1997@hotmail.com>
2024-05-08 11:38:18 -0300

    feat: adding icon to sideBar when cart lenth > 0

--- src/components/cart/sidebar.tsx
+++ src/components/cart/sidebar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Sheet,
   SheetContent,
@@ -8,29 +9,45 @@ import {
 import { Button } from "@/components/ui/button";
 import { RocketIcon } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
+import { useCartStore } from "@/stores/cart-store";
+import { CartItem } from "@/components/cart/cart-item";
 
 export const CartSidebar = () => {
+  const { cart } = useCartStore((state) => state);
+
+  let subTotal = 0;
+
+  for (let item of cart) {
+    subTotal += item.product.price * item.quantity;
+  }
   return (
     <Sheet>
       <SheetTrigger asChild>
-        <Button className="flex items-center gap-1">
+        <Button className="flex items-center gap-1 relative">
           <RocketIcon />
           <span className="block">Carrinho</span>
+          {cart.length > 0 && (
+            <div className="absolute size-3 bg-red-600 rounded-full -right-1 -top-1"></div>
+          )}
         </Button>
       </SheetTrigger>
       <SheetContent>
         <SheetHeader>
           <SheetTitle>Carrinho</SheetTitle>
         </SheetHeader>
-        <div className="flex flex-col gap-5 my-3">...</div>
+        <div className="flex flex-col gap-5 my-3">
+          {cart.map((item) => (
+            <CartItem key={item.product.id} item={item} />
+          ))}
+        </div>
         <Separator className="my-4" />
         <div className="flex justify-between items-center text-xs">
           <div>Subtotal</div>
-          <div>...</div>
+          <div>R$ {subTotal.toFixed(2)}</div>
         </div>
         <Separator className="my-4" />
         <div className="text-center">
-          <Button>Finalizar compra</Button>
+          <Button disabled={cart.length === 0}>Finalizar compra</Button>
         </div>
       </SheetContent>
     </Sheet>
