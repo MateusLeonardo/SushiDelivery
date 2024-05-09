8e1237d7e4322ddf45fbccad6c1a33428f7879ec
MateusLeonardo <mateus_leonardo1997@hotmail.com>
2024-05-09 13:57:19 -0300

    feat: adding cart-item logic

--- src/components/cart/cart-item.tsx
+++ src/components/cart/cart-item.tsx
@@ -1,9 +1,27 @@
 import { Cart } from "@/types/cart";
+import { CartItemQuantity } from "@/components/cart/item-quantity";
 
 type Props = {
   item: Cart;
 };
 
 export const CartItem = ({ item }: Props) => {
-  return <div>...</div>;
+  return (
+    <div className="flex items-center gap-5">
+      <div className="w-16 overflow-hidden rounded-sm">
+        <img
+          src={item.product.image}
+          alt={item.product.name}
+          className="w-full h-auto object-cover"
+        />
+      </div>
+      <div className="flex-1">
+        <p className="text-base">{item.product.name}</p>
+        <p className="text-xs opacity-50">R$ {item.product.price.toFixed(2)}</p>
+      </div>
+      <div>
+        <CartItemQuantity cartItem={item} />
+      </div>
+    </div>
+  );
 };
