b7c361b1a95035806d817fbfa07099704d94c86e
mateus leonardo <mateus_leonardo1997@hotmail.com>
2024-05-11 15:35:45 -0300

    feat: adding checkoutStore

---
+++ src/stores/checkout-store.ts
@@ -0,0 +1,35 @@
+import { create } from "zustand";
+
+type States = {
+  name: string;
+  adress: {
+    street: string;
+    number: string;
+    complement?: string | undefined;
+    district: string;
+    city: string;
+    state: string;
+  };
+};
+
+type Actions = {
+  setName: (name: States["name"]) => void;
+  setAdress: (adress: States["adress"]) => void;
+};
+
+const initialState: States = {
+  name: "",
+  adress: {
+    street: "",
+    number: "",
+    district: "",
+    complement: "",
+    city: "",
+    state: "",
+  },
+};
+export const useCheckoutStore = create<States & Actions>()((set) => ({
+  ...initialState,
+  setName: (name) => set((state) => ({ ...state, name })),
+  setAdress: (adress) => set((state) => ({ ...state, adress })),
+}));
