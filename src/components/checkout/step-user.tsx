04090731671dc5711d99026291839e879e66879a
mateus leonardo <mateus_leonardo1997@hotmail.com>
2024-05-13 13:02:33 -0300

    alter: changing component import

--- src/components/checkout/step-user.tsx
+++ src/components/checkout/step-user.tsx
@@ -4,9 +4,15 @@ import { Dispatch, SetStateAction } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Form, FormControl, FormField, FormItem, FormLabel } from "../ui/form";
-import { Input } from "../ui/input";
-import { Button } from "../ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 type Props = {
   setSteps: Dispatch<SetStateAction<CheckoutSteps>>;
