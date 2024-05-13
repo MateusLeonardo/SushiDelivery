b5ef8c3c852fb8d62bae376d46ee005b336fbc11
mateus leonardo <mateus_leonardo1997@hotmail.com>
2024-05-13 13:03:13 -0300

    feat: adding logic to the stepAdress component

--- src/components/checkout/step-adress.tsx
+++ src/components/checkout/step-adress.tsx
@@ -1,14 +1,148 @@
+import { useCheckoutStore } from "@/stores/checkout-store";
 import { CheckoutSteps } from "@/types/checkout-steps";
 import { Dispatch, SetStateAction } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 type Props = {
   setSteps: Dispatch<SetStateAction<CheckoutSteps>>;
 };
 
+const formSchema = z.object({
+  street: z.string().min(2, "Preencha o endereço"),
+  number: z.string().min(2, "Preencha o endereço"),
+  complement: z.string().min(2),
+  district: z.string().min(2, "Preencha o bairro"),
+  city: z.string().min(2, "Preencha a cidade"),
+  state: z.string().min(2, "Preencha o estado"),
+});
+
 export const StepAdress = ({ setSteps }: Props) => {
+  const { adress, setAdress } = useCheckoutStore((state) => state);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: { ...adress },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    setAdress(values);
+    setSteps("finish");
+  };
+
   return (
-    <div>
-      <h1>Adress</h1>
-    </div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <div className="grid grid-cols-2 gap-4">
+          <FormField
+            control={form.control}
+            name="street"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Rua</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="number"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Número</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="complement"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Complemento</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="district"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Bairro</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="city"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Cidade</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="state"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Estado</FormLabel>
+                <FormControl>
+                  <Select
+                    defaultValue={field.value}
+                    onValueChange={field.onChange}
+                  >
+                    <SelectTrigger>
+                      <SelectValue placeholder="Estado" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="sp">São Paulo</SelectItem>
+                      <SelectItem value="mg">Minas Gerais</SelectItem>
+                      <SelectItem value="pr">Paraná</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </FormControl>
+              </FormItem>
+            )}
+          />
+        </div>
+        <div className="flex justify-between mt-4">
+          <Button variant="link" onClick={() => setSteps("user")}>
+            Voltar
+          </Button>
+          <Button type="submit">Próximo</Button>
+        </div>
+      </form>
+    </Form>
   );
 };
