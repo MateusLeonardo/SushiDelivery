40fd410acbd98732cf0cb68627545ff92f86b727
mateus leonardo <mateus_leonardo1997@hotmail.com>
2024-05-14 23:28:10 -0300

    feat: adding initial logic to send the request via WhatsApp

--- src/components/checkout/step-finish.tsx
+++ src/components/checkout/step-finish.tsx
@@ -1,7 +1,27 @@
+import { useCheckoutStore } from "@/stores/checkout-store";
+import { Button } from "../ui/button";
+import Link from "next/link";
+
 export const StepFinish = () => {
+  const { name } = useCheckoutStore((state) => state);
+
+  const message = "";
+  const linkZap = `https://wa.me//${
+    process.env.NEXT_PUBLIC_ZAP
+  }?text=${encodeURI(message)}`;
+
   return (
-    <div>
-      <h1>Finish</h1>
+    <div className="text-center flex flex-col gap-5">
+      <p>Perfeito</p>
+      <p>
+        Agora envie seu pedido ao nosso WhatsApp para concluir. Nosso atendente
+        irá te guiar sobre o andamento do pedido
+      </p>
+      <Button>
+        <Link target="_blank" href={linkZap}>
+          Enviar para o WhatsApp
+        </Link>
+      </Button>
     </div>
   );
 };
